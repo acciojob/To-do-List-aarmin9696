2ea2fe96eca19240757ad444bea6e86c739f4d3d
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-18 00:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+window.addEventListener('DOMContentLoaded', () => {
+  const newTodoInput = document.getElementById('newTodoInput');
+  const addTodoBtn = document.getElementById('addTodoBtn');
+  const todoList = document.getElementById('todoList');
+
+  addTodoBtn.addEventListener('click', () => {
+    const todoText = newTodoInput.value.trim();
+
+    if (todoText !== '') {
+      const newTodoItem = document.createElement('li');
+      newTodoItem.textContent = todoText;
+      todoList.appendChild(newTodoItem);
+
+      newTodoInput.value = '';
+    }
+  });
+
+  newTodoInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      addTodoBtn.click();
+    }
+  });
+});
